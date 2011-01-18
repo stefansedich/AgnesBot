e9fc148b0efc1c7b11ad1458d6b657d2a44976e3
Stefan Sedich <stefan.sedich@gmail.com>
2011-01-18 12:58:29 +1000

    Adding date module

---
+++ src/AgnesBot.Modules.DateModule.Tests/DateModuleFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AgnesBot.Core.IrcUtils;
+using AgnesBot.Core.Utils;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace AgnesBot.Modules.DateModule.Tests
+{
+    [TestFixture]
+    public class DateModuleFixture
+    {
+        private DateModule _module;
+        private IIrcClient _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _client = MockRepository.GenerateStub<IIrcClient>();
+            _module = new DateModule(_client);
+        }
+
+        [Test]
+        public void Can_Display_Date()
+        {
+            // Arrange
+            var data = new IrcMessageData {Channel = "#test", Type = ReceiveType.ChannelMessage, Message = "!date"};
+
+            SystemTime.Now = () => new DateTime(2009, 1, 1, 10, 22, 34);
+
+            // Act
+            _module.Process(data);
+
+            // Assert
+            string expected = SystemTime.Now().ToString("yyyy-MM-dd hh:mm:sstt");
+
+            _client.AssertWasCalled(client => client.SendMessage(SendType.Message, data.Channel, expected));
+        }
+    }
+}
---
+++ src/AgnesBot.Modules.DateModule/DateModule.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using AgnesBot.Core.IrcUtils;
+using AgnesBot.Core.Modules;
+using AgnesBot.Core.Utils;
+
+namespace AgnesBot.Modules.DateModule
+{
+    public class DateModule : BaseModule
+    {
+        public DateModule(IIrcClient client) : base(client)
+        {
+            AddHandler(new ModuleMessageHandler
+                           {
+                               Type = ReceiveType.ChannelMessage,
+                               CommandRegex = new Regex("^!date$"),
+                               Method = "DisplayDate"
+                           });
+        }
+
+        protected void DisplayDate(IrcMessageData data)
+        {
+            string date = SystemTime.Now().ToString("yyyy-MM-dd hh:mm:sstt");
+
+            Client.SendMessage(SendType.Message, data.Channel, date);
+        }
+    }
+}
